6ae5f2d4164e2d8224a4f6c78c9fa67d5bfd828d
nickxin <nickxin@userdeMac-mini.local>
2018-01-26 13:48:23 +0800

    remove date picker

--- src/app/modules/system/apps/research/research.module.ts
+++ src/app/modules/system/apps/research/research.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
-import {DpDatePickerModule} from 'ng2-date-picker';
 import { HomeComponent } from './home.component'
 import { SystemModule } from '../../system.module'
 import { ThemeModule } from '../../../../@theme/theme.module';
