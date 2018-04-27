fc491113d23001baa6011427cc0d07833d81f23d
Andy Sun <jsun@kenexa.com.cn>
2018-04-27 17:52:36 +0800

    code format

--- src/app/modules/system/config.ts
+++ src/app/modules/system/config.ts
@@ -1,11 +1,11 @@
-import {ProductService, CategoryService, TagService, OrderLogsService} from './apps/product/config';
-import {UserInfoService, DepartmentService, UserRoleService, AuthNodeService, AuthorizeService} from './apps/account/config';
-import {AttachmentService} from './apps/uploader/config';
-import {NavService} from './apps/home/config';
-import {ResearchService} from './apps/research/config';
-import {TemplateService, EmailService, SmsService, WinningService} from './apps/notification/config';
-import {QuestionService} from './apps/question/config';
-import {ParameterService} from './apps/dataModel/config';
+import { ProductService, CategoryService, TagService, OrderLogsService } from './apps/product/config';
+import { UserInfoService, DepartmentService, UserRoleService, AuthNodeService, AuthorizeService } from './apps/account/config';
+import { AttachmentService } from './apps/uploader/config';
+import { NavService } from './apps/home/config';
+import { ResearchService } from './apps/research/config';
+import { TemplateService, EmailService, SmsService, WinningService } from './apps/notification/config';
+import { QuestionService } from './apps/question/config';
+import { ParameterService } from './apps/dataModel/config';
 
 export const ModuleConfig = {
     product: {
--- src/app/modules/system/system.component.ts
+++ src/app/modules/system/system.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Routes, RouterModule,ActivatedRoute,Router } from '@angular/router';
+import { Routes, RouterModule, ActivatedRoute, Router } from '@angular/router';
 import { NavService } from '../../@core/data/nav.service';
 import { TranslateService } from '../../@core/utils/translate.service';
 import { HttpService } from '../../@core/utils/resource.service';
@@ -15,28 +15,28 @@ import { AuthService, NbAuthResult } from '../../modules/auth/services/auth.serv
 export class SystemComponent {
   menu = [];
   intervalId
-  constructor(public route: ActivatedRoute, protected service: AuthService,public router: Router,public httpService:HttpService,public navService:NavService,public translateService:TranslateService ) {
-       console.log("system enter")
-       this.intervalId = setInterval(()=>{
-           this.httpService.get("/home/heartbeart",{}).map(res=>res.json()).subscribe(res=>{
-               if(res.data.isLogin == false){
-                    this.service.logout("email").subscribe((result: NbAuthResult) => {
-                        const redirect = result.getRedirect();
-                        if (redirect) {
-                          setTimeout(() => {
-                            return this.router.navigateByUrl(redirect);
-                          }, 0);
-                        }
-                    });
-               }
-           })
-       },60000)
+  constructor(public route: ActivatedRoute, protected service: AuthService, public router: Router, public httpService: HttpService, public navService: NavService, public translateService: TranslateService) {
+    console.log("system enter")
+    this.intervalId = setInterval(() => {
+      this.httpService.get("/home/heartbeart", {}).map(res => res.json()).subscribe(res => {
+        if (res.data.isLogin == false) {
+          this.service.logout("email").subscribe((result: NbAuthResult) => {
+            const redirect = result.getRedirect();
+            if (redirect) {
+              setTimeout(() => {
+                return this.router.navigateByUrl(redirect);
+              }, 0);
+            }
+          });
+        }
+      })
+    }, 60000)
   }
 
 
-  ngOnDestroy(){
-       if(this.intervalId){
-           clearInterval(this.intervalId)
-       }
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId)
+    }
   }
 }
